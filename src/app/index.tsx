26d79dcf6c7eedbbe2ddc4ee9b4d91b04e04b29f
Shahriar Anan <shahriaranany6@gmail.com>
2026-03-24 05:12:46 +0600

    chore: initialize expo project with folder structure

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+export default function App() {
+    return (
+        <>
+            {/* Your app content goes here */}
+            <View>
+                <Text className="text-blue-900 text-lg font-bold">Hello, World! My</Text>
+            </View>
+        </>
+    );
+}
